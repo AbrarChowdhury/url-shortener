84f8b4b1a76d9b88bdb97267200d8e73e58f00aa
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2022-04-14 12:18:45 +0600

    initalize all components
    style: finish the footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Hero/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
