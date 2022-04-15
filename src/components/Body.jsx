e3dbc1250947fe5f27cc01a5332449995f546c94
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2022-04-15 17:28:58 +0600

    create cards

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import Card from './card/Card'
-
+import bRIcon from "../images/icon-brand-recognition.svg"
+import dRIcon from "../images/icon-detailed-records.svg"
+import fCIcon from "../images/icon-fully-customizable.svg"
+import { Grid } from '@mui/material'
 function Body() {
   return (
-    <div>body</div>
+    <div id="body" className='container'>body
+    <div id="cards-container">
+    <Grid container spacing={4}>
+      <Grid item xs={12} lg={4}>
+        <Card
+          icon={bRIcon} 
+          title="Brand Recognition"
+          text="Boost your brand recognition with each click. Generic links doen't mean a thing. Branded links help instil confidence in your content."
+        /> 
+      </Grid>
+      <Grid item xs={12} lg={4}>
+        <Card
+          icon={dRIcon} 
+          title="Detailed Records"
+          text="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
+        /> 
+      </Grid>
+      <Grid item xs={12} lg={4}>
+        <Card
+          icon={fCIcon} 
+          title="Fully Customizable"
+          text="improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+        /> 
+      </Grid>
+    </Grid>
+    </div>
+
+    </div>
   )
 }
 
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,19 @@
+import { Paper } from '@mui/material'
 import React from 'react'
 import "./card.style.scss"
 function Card({icon,title,text}) {
   return (
     <div>
-      <h2>{title}</h2>
-      <p>{text}</p>
+    <br />
+    <Paper elevation={1} style={{height:'100%'}}>
+      <div className='card'>
+        <div className='image-container'>
+          <img src={icon} />
+        </div>
+        <h2>{title}</h2>
+        <p>{text}</p>
+      </div>
+    </Paper>
     </div>
   )
 }
